84a5d6d268d080dfb46086b1ab081e45b6891925
Gerry Blanch <g.blandugar@gmail.com>
2024-01-16 19:54:39 -0500

    main body

--- myApp.js
+++ myApp.js
@@ -4,9 +4,18 @@ const app = express();
 
 
 
-
-
-
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let PORT = process.env.PORT || 3035;
+app.listen(port, () => {
+  console.log(`The server started on port ${port}`);
+});
 
 
 
