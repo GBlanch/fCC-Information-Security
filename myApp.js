885be852bf63aab6c615b7655362334b68ab234d
Gerry Blanch <g.blandugar@gmail.com>
2024-01-19 19:09:42 -0500

    dnsPfC middleware fixed no apply method

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ const ninetyDaysInSeconds = 90*24*60*60;
 
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
-app.use.apply(helmet.dnsPrefetchControl());
+app.use(helmet.dnsPrefetchControl());
 
 
 module.exports = app;
