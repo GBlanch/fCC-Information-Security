505b56e71bbdce12e3212f59d896e9faa1677a32
Gerry Blanch <g.blandugar@gmail.com>
2024-01-19 19:20:23 -0500

    contentSP middleware added

--- myApp.js
+++ myApp.js
@@ -20,6 +20,15 @@ app.use(helmet.dnsPrefetchControl());
 
 app.use(helmet.noCache());
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+    defaultSrc: ['self'],
+    scriptSrc: ['self', 'trusted-cdn.com']
+    }
+  })
+);
+
 
 module.exports = app;
 const api = require('./server.js');
